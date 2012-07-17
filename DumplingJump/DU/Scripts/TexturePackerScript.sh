760fe49f89d57bbbe271e443e85e2f5b72f66633
Zero <zerolxy@gmail.com>
2012-07-16 21:01:46 -0700

    add Assets and Scripts

---
+++ DumplingJump/DU/Scripts/TexturePackerScript.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+TP="/usr/local/bin/TexturePacker"
+RESOURCES_PATH="${SRCROOT}/DumplingJump/DU/Resources"
+ASSETS_PATH="${SRCROOT}/DumplingJump/DU/Assets"
+
+if [ "${ACTION}" = "clean" ]
+then
+    echo "cleaning..."
+    rm DumplingJump/DU/Resources/spriteSheets/*.png
+    rm DumplingJump/DU/Resources/spriteSheets/*.plist
+    
+    # ....
+    # add all files to be removed in clean phase
+    # ....
+else
+    echo "building..."
+    
+    ${TP} DumplingJump/DU/Assets/hero.tps
+fi
+exit 0

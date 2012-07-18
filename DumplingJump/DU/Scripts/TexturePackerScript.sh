bd8179db17bac25a1717423eca6924e11e426a16
Zero <zerolxy@gmail.com>
2012-07-17 22:40:55 -0700

    Updated the texture packer script

--- DumplingJump/DU/Scripts/TexturePackerScript.sh
+++ DumplingJump/DU/Scripts/TexturePackerScript.sh
@@ -6,18 +6,19 @@ ASSETS_PATH="${SRCROOT}/DumplingJump/DU/Assets"
 
 if [ "${ACTION}" = "clean" ]
 then
-    echo "cleaning..."
-    rm DumplingJump/DU/Resources/spriteSheets/*.png
-    rm DumplingJump/DU/Resources/spriteSheets/*.pvr
-    rm DumplingJump/DU/Resources/spriteSheets/*.plist
-    
-    # ....
-    # add all files to be removed in clean phase
-    # ....
+echo "cleaning..."
+rm DumplingJump/DU/Resources/spriteSheets/*.png
+rm DumplingJump/DU/Resources/spriteSheets/*.pvr
+rm DumplingJump/DU/Resources/spriteSheets/*.plist
+rm -rf DumplingJump/DU/Resources/spriteSheets
+# ....
+# add all files to be removed in clean phase
+# ....
 else
-    echo "building..."
-    
-    ${TP} DumplingJump/DU/Assets/hero.tps
-    ${TP} DumplingJump/DU/Assets/background.tps
+echo "building..."
+
+mkdir -p DumplingJump/DU/Resources/spriteSheets
+${TP} DumplingJump/DU/Assets/hero.tps
+${TP} DumplingJump/DU/Assets/background.tps
 fi
 exit 0
